977e3858bd5b8fc8f49cc1c148252167630ef477
parkjunghun <go9018@naver.com>
2020-03-01 20:48:26 +0900

    add : get user post count / Hero Widget use animation

--- lib/Insta_account_page.dart
+++ lib/Insta_account_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AccountPage extends StatefulWidget {
-
   final FirebaseUser user;
 
   AccountPage(this.user);
@@ -14,6 +14,23 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  int _postCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Firestore.instance
+        .collection('post')
+        .where('email', isEqualTo: widget.user.email)
+        .getDocuments()
+        .then((snapShot) {
+      setState(() {
+        _postCount = snapShot.documents.length;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +42,12 @@ class _AccountPageState extends State<AccountPage> {
   Widget _buildAppBar() {
     return AppBar(
       actions: <Widget>[
-        IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {
-          FirebaseAuth.instance.signOut();
-          _googleSignIn.signOut();
-        })
+        IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              _googleSignIn.signOut();
+            })
       ],
     );
   }
@@ -48,37 +67,35 @@ class _AccountPageState extends State<AccountPage> {
                     width: 80.0,
                     height: 80.0,
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          widget.user.photoUrl),
+                      backgroundImage: NetworkImage(widget.user.photoUrl),
                     ),
                   ),
                   Container(
-                    width: 80.0,
-                    height: 80.0,
-                    alignment: Alignment.bottomRight,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: <Widget>[
-                        SizedBox(
-                          width: 28.0,
-                          height: 28.0,
-                          child: FloatingActionButton(
-                            backgroundColor: Colors.white,
-                            onPressed: () {},
+                      width: 80.0,
+                      height: 80.0,
+                      alignment: Alignment.bottomRight,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 28.0,
+                            height: 28.0,
+                            child: FloatingActionButton(
+                              backgroundColor: Colors.white,
+                              onPressed: () {},
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 25.0,
-                          height: 25.0,
-                          child: FloatingActionButton(
-                            backgroundColor: Colors.blue,
-                            child: Icon(Icons.add),
-                            onPressed: () {},
+                          SizedBox(
+                            width: 25.0,
+                            height: 25.0,
+                            child: FloatingActionButton(
+                              backgroundColor: Colors.blue,
+                              child: Icon(Icons.add),
+                              onPressed: () {},
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  )
+                        ],
+                      ))
                 ],
               ),
               Padding(
@@ -91,7 +108,7 @@ class _AccountPageState extends State<AccountPage> {
             ],
           ),
           Text(
-            '0\게시물',
+            '$_postCount\n게시물',
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 18.0),
           ),
