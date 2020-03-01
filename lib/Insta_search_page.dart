25f6c9cd1dd25e3414799650d761c7415289f460
parkjunghun <go9018@naver.com>
2020-03-01 20:17:36 +0900

    add : get firestore img datasnapshot

--- lib/Insta_search_page.dart
+++ lib/Insta_search_page.dart
@@ -1,9 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/Insta_create_page.dart';
 
 class SearchPage extends StatefulWidget {
-
   final FirebaseUser user;
 
   SearchPage(this.user);
@@ -22,26 +22,41 @@ class _SearchPageState extends State<SearchPage> {
         child: Icon(Icons.create),
         backgroundColor: Colors.blue,
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePage(widget.user)));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CreatePage(widget.user)));
         },
       ),
     );
   }
 
   Widget _buildBody() {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 1.0,
-            mainAxisSpacing: 1.0,
-            crossAxisSpacing: 1.0),
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return _buildListItems(context, index);
-        });
+    return StreamBuilder(
+      stream: Firestore.instance.collection('post').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        var items = snapshot.data.documents ?? [];
+
+        return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0),
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return _buildListItems(context, items[index]);
+            });
+      },
+    );
   }
 
-  Widget _buildListItems(BuildContext context, int index) {
-    return Image.network('https://www.dreamsquadgroup.com/wp-content/uploads/2019/04/maxresdefault-1080x675.jpg', fit: BoxFit.cover,);
+  Widget _buildListItems(BuildContext context, document) {
+    return Image.network(
+      document['photoUrl'],
+      fit: BoxFit.cover,
+    );
   }
 }
